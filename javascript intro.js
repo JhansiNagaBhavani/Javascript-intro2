a01f44939d873cc3cfded03c6238958b72b8fdfe
KODURI JHANSI NAGA BHAVANI <kodurijhansi441@gmail.com>
2025-07-12 10:19:43 +0530

    Add files via upload

---
+++ javascript intro.js
@@ -0,0 +1,9 @@
+ function showMessage() {
+      alert("Hello from Internal Script");
+    }
+
+console.log("Hello World");               // Standard log
+console.info("Hello World");              // Info message
+console.warn("Hello World");              // Warning message
+console.error("Hello World");             // Error message
+console.debug("Hello World");             // Debug message
